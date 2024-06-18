37612b80bd47b22cfdf85262de5a9d9e18dcc5ce
Firomt <firomsamt@gmail.com>
2024-06-19 02:15:57 +0300

    make RegisterUser, Session and Tag controllers

--- jobs-platform/resources/views/auth/register.blade.php
+++ jobs-platform/resources/views/auth/register.blade.php
@@ -2,17 +2,16 @@
     <x-page-heading>Register</x-page-heading>
 
     <x-forms.form method="POST" action="/register" enctype="multipart/form-data">
-        <x-forms.input label="Your Name" name="name" />
+        <x-forms.input label="Name" name="name" />
         <x-forms.input label="Email" name="email" type="email" />
-        <x-forms.input label="Password" name="Password" type="password"/>
-        <x-forms.input label="Password Confirmation" name="password_confirmation"  type="password"/>
-
-        <x-forms.button>Create Account</x-forms.button>
+        <x-forms.input label="Password" name="password" type="password" />
+        <x-forms.input label="Password Confirmation" name="password_confirmation" type="password" />
 
         <x-forms.divider />
+
         <x-forms.input label="Employer Name" name="employer" />
-        <x-forms.input label="Employer Logo" name="logo" type="file"/>
+        <x-forms.input label="Employer Logo" name="logo" type="file" />
 
-
+        <x-forms.button>Create Account</x-forms.button>
     </x-forms.form>
 </x-layout>
--- jobs-platform/resources/views/components/job-card-wide.blade.php
+++ jobs-platform/resources/views/components/job-card-wide.blade.php
@@ -3,19 +3,24 @@
 <x-panel class="flex gap-x-6">
     <div>
         <x-employer-logo />
+            
     </div>
 
     <div class="flex-1 flex flex-col">
         <a href="#" class="self-start text-sm text-gray-400 transition-colors duration-300">{{ $job->employer->name }}</a>
 
-        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-800">{{ $job->title }}</h3>
+        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-800">
+            <a href="{{ $job->url }}" target="_blank">
+                {{ $job->title }}
+            </a>
+        </h3>
 
         <p class="text-sm text-gray-400 mt-auto">{{ $job->salary }}</p>
     </div>
 
     <div>
         @foreach($job->tags as $tag)
-          <x-tag :$tag/>
+            <x-tag :$tag />
         @endforeach
     </div>
 </x-panel>
